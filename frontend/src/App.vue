efbdfaeff16f2de897e0c7ab80bf70c59d4981c4
Löris <101009352+lorige55@users.noreply.github.com>
2024-08-19 19:09:15 +0200

    Added basic (nonworking) UI

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,34 @@
-<script setup lang="ts"></script>
+<script>
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
 
-<template></template>
+export default {
+  components: {
+    Textarea,
+    Button,
+  },
+  data() {
+    return {
+      text: "",
+    };
+  },
+  mounted() {
+    //
+  },
+  watch: {
+    //
+  },
+  methods: {
+    //
+  },
+};
+</script>
+
+<template>
+  <div class="grid h-screen place-items-center">
+    <div class="grid w-full gap-2 w-2/3">
+      <Textarea v-model="text" placeholder="Type your text here." />
+      <Button>Fancyficate</Button>
+    </div>
+  </div>
+</template>
