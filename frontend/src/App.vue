6622ae778322137aa21b19deb68fd949738a84f0
Löris <101009352+lorige55@users.noreply.github.com>
2024-08-23 17:00:50 +0200

    Added clipboard button and added words

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,14 @@
 <script>
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
-import { Loader2 } from "lucide-vue-next";
+import { Loader2, Copy } from "lucide-vue-next";
 
 export default {
   components: {
     Textarea,
     Button,
     Loader2,
+    Copy,
   },
   data() {
     return {
@@ -39,6 +40,9 @@ export default {
         });
       this.fancyficating = false;
     },
+    copyText() {
+      navigator.clipboard.writeText(this.text);
+    },
   },
 };
 </script>
@@ -47,15 +51,23 @@ export default {
   <div class="grid h-screen place-items-center">
     <div class="grid w-2/3 gap-2">
       <Textarea
+        class="h-64"
         :disabled="fancyficating ? true : false"
         v-model="text"
         placeholder="Type your text here."
       />
-      <Button v-if="!fancyficating" @click="fancyficate()">Fancyficate</Button>
-      <Button v-else disabled>
-        <Loader2 class="w-4 h-4 mr-2 animate-spin" />
-        Please wait
-      </Button>
+      <div class="w-full flex">
+        <Button variant="outline" size="icon" class="mr-2" @click="copyText()">
+          <Copy class="w-4 h-4" />
+        </Button>
+        <Button v-if="!fancyficating" @click="fancyficate()" class="w-full"
+          >Fancyficate</Button
+        >
+        <Button v-else disabled class="w-full">
+          <Loader2 class="w-4 h-4 mr-2 animate-spin" />
+          Please wait
+        </Button>
+      </div>
     </div>
   </div>
 </template>
