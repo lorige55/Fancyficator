e8ac684f33613aca046bb2d4f4f3c51bfb00af3c
Löris <101009352+lorige55@users.noreply.github.com>
2024-08-25 12:21:27 +0200

    UI changes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -56,30 +56,36 @@ export default {
 
 <template>
   <div class="grid h-screen place-items-center">
-    <div class="grid w-2/3 gap-2">
-      <Textarea
-        class="h-64"
-        :disabled="fancyficating ? true : false"
-        v-model="text"
-        placeholder="Type your text here."
-      />
-      <div class="w-full flex">
-        <Button variant="outline" size="icon" class="mr-2" @click="copyText()">
-          <Copy class="w-4 h-4" />
-        </Button>
-        <Button v-if="!fancyficating" @click="fancyficate()" class="w-full"
-          >Fancyficate</Button
-        >
-        <Button v-else disabled class="w-full">
-          <Loader2 class="w-4 h-4 mr-2 animate-spin" />
-          Please wait
-        </Button>
+    <div class="flex w-2/3 gap-2">
+      <div class="flex flex-col w-full gap-2">
+        <Textarea
+          class="h-64"
+          :disabled="fancyficating ? true : false"
+          v-model="text"
+          placeholder="Type your text here."
+        />
+        <div class="w-full flex">
+          <Button
+            variant="outline"
+            size="icon"
+            class="mr-2"
+            @click="copyText()"
+          >
+            <Copy class="w-4 h-4" />
+          </Button>
+          <Button v-if="!fancyficating" @click="fancyficate()" class="w-full">
+            Fancyficate
+          </Button>
+          <Button v-else disabled class="w-full">
+            <Loader2 class="w-4 h-4 mr-2 animate-spin" />
+            Please wait
+          </Button>
+        </div>
       </div>
-      <ScrollArea class="h-72 w-48 rounded-md border" v-if="changed !== null">
+      <ScrollArea class="w-64 rounded-md border" v-if="changed !== null">
         <div class="p-4">
           <h4 class="mb-4 text-sm font-medium leading-none">Changed</h4>
-
-          <div v-for="word in changed">
+          <div v-for="word in changed" :key="word.word">
             <div class="text-sm">
               {{ word.synonym }} <ArrowBigRight class="w-4 h-4 inline" />
               {{ word.word }}
