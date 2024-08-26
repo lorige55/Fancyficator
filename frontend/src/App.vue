71199960b4ef5db60754f2909ab527b4b1ef00b1
Löris <101009352+lorige55@users.noreply.github.com>
2024-08-26 20:46:29 +0200

    UI changes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,6 +3,14 @@ import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { Loader2, Copy, ArrowBigRight } from "lucide-vue-next";
 
 export default {
@@ -14,6 +22,12 @@ export default {
     ScrollArea,
     Separator,
     ArrowBigRight,
+    Label,
+    Switch,
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
   },
   data() {
     return {
@@ -59,46 +73,69 @@ export default {
     <div class="w-screen">
       <h1 class="text-lg font-semibold m-5">Fancyficator</h1>
     </div>
-    <Separator class="my-4" />
+    <Separator />
     <div class="grid m-5 h-full">
       <div class="flex h-full gap-2">
         <div class="flex flex-col w-full gap-2">
           <Textarea
-            class="w-full flex flex-grow"
+            class="w-full flex flex-grow mb-2"
             :disabled="fancyficating ? true : false"
             v-model="text"
             placeholder="Type your text here."
           />
           <div class="w-full flex">
-            <Button v-if="!fancyficating" @click="fancyficate()">
+            <Button
+              class="shadow-sm"
+              v-if="!fancyficating"
+              @click="fancyficate()"
+            >
               Fancyficate
             </Button>
-            <Button v-else disabled>
+            <Button class="shadow-sm" v-else disabled>
               <Loader2 class="w-4 h-4 mr-2 animate-spin" />
               Please wait
             </Button>
             <Button
               variant="outline"
               size="icon"
-              class="ml-2"
+              class="ml-2 shadow-sm"
               @click="copyText()"
             >
               <Copy class="w-4 h-4" />
             </Button>
           </div>
         </div>
-        <ScrollArea class="w-64 rounded-md border" v-if="changed !== null">
-          <div class="p-4">
-            <h4 class="mb-4 text-sm font-medium leading-none">Changed</h4>
-            <div v-for="word in changed" :key="word.word">
-              <div class="text-sm">
-                {{ word.synonym }} <ArrowBigRight class="w-4 h-4 inline" />
-                {{ word.word }}
+        <div class="w-96">
+          <h4 class="ml-4 text-sm font-medium leading-none">Settings</h4>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <div class="p-4 flex items-center space-x-2">
+                  <Switch disabled checked id="vocab" />
+                  <Label for="vocab">Replace Vocabulary</Label>
+                </div></TooltipTrigger
+              >
+              <TooltipContent class="w-64">
+                <p>
+                  <i>Fancyficate</i> your text with overly posh vocabulary.
+                  Wouldn't use it for a real, grades essay for school...
+                </p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          <ScrollArea class="w-full">
+            <div class="p-4">
+              <h4 class="mb-4 text-sm font-medium leading-none">Changed</h4>
+              <div v-for="word in changed" :key="word.word">
+                <div class="text-sm">
+                  {{ word.synonym }} <ArrowBigRight class="w-4 h-4 inline" />
+                  {{ word.word }}
+                </div>
+                <Separator class="my-2" />
               </div>
-              <Separator class="my-2" />
             </div>
-          </div>
-        </ScrollArea>
+          </ScrollArea>
+        </div>
       </div>
     </div>
   </div>
