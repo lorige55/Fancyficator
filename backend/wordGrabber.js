96566bcf1d9140e1e27e3ef458bb27b0be48a0cb
lorige55 <loris.laetsch@gmail.com>
2024-08-31 15:39:06 +0200

    Add words

--- backend/wordGrabber.js
+++ backend/wordGrabber.js
@@ -29,7 +29,7 @@ async function llama3(prompt) {
 async function generateWords() {
   console.log("Generating word");
   let word = await llama3(
-    `Generate a random word that is considered overly fancy, posh, or sophisticated. Ensure that it is uncommon, obscure, and difficult for an average person to understand. Return only the word, without any capitalization, and without any additional text or explanation.`
+    `Generate a random word that is considered overly fancy, posh, or sophisticated. Ensure that the word itself is uncommon, obscure, and difficult for an average person to understand but the synonym of it is common. Thus a fancy word for something really basic. Return only the word, without any capitalization, and without any additional text or explanation.`
   );
   console.log("Got word: " + word);
   if (existingWords.includes(word)) {
@@ -41,8 +41,14 @@ async function generateWords() {
   existingWords.push(word);
   console.log("Awaiting synonyms");
   let synonyms = await llama3(
-    `Give me as many synonyms as possible for the following word: ${word} Respond in this syntax: synonym, synonym, synonym. Use a comma after every synonym, except the last one. Do not capitalize any of the synonyms. Do not respond with anyhting else.`
+    `Give me as many synonyms as possible for the following word: ${word} Respond in this syntax: synonym, synonym, synonym. Use a comma after every synonym, except the last one. Do not capitalize any of the synonyms. Do not respond with anyhting else. Do not use quotes or punctuation in your response. If there is a problem, for example if you couldn't find any synonyms, respond with just null. Do not respond with anything else under any circumstances.`
   );
+  if (synonyms == "null") {
+    console.log("No synonyms found, trying again");
+    console.log("----------------------------------------");
+    generateWords();
+    return;
+  }
   console.log(synonyms);
   synonyms = synonyms.split(", ");
   console.log("Got synonyms: " + synonyms);
